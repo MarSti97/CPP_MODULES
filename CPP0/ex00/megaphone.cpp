748e92a592d153f733c47c97e9e52597b6a3c893
katarina gajic <Katarina1@MacBook-Air.local>
2023-06-14 21:42:27 +0100

    added megaphone first version from mac

---
+++ CPP0/ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    if (ac == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else 
+    {
+        for (int i = 2; i <= ac; i++)
+        {
+            // std::cout << "HERE\n";
+            int c = -1;
+            while (av[i - 1][++c])
+            {
+                char r = toupper(av[i - 1][c]);
+                std::cout << r;           
+            }
+        }
+        std::cout << std::endl;
+    }
+    return (0);
+}
