a8a3b1a4d4929d88af04b00d1a039ed275619b55
katarina gajic <Katarina1@MacBook-Air.local>
2023-06-26 11:13:25 +0100

    ex04 finished, working, run tests

--- CPP1/ex04/main.cpp
+++ CPP1/ex04/main.cpp
@@ -1,38 +1,43 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <cstdio>
 
 int main(int ac, char **av)
 {
-    if (ac != 4)
+    if (ac != 3)
     {
-        FILE* infile = std::fopen(av[1], "r");
+        std::ifstream infile(av[1]);
         if (infile == NULL)
         {
             std::perror("Error: file can't be opened");
-            std::fclose(infile);
+            infile.close();
             return (1);
         }
-        std::string filename = av[1] + ".replace"
-        FILE* outfile = std::fopen(filename, "w");
+        std::string filename(av[1]);
+        filename += ".replace";
+        std::ofstream outfile(filename);
         if (outfile == NULL)
         {
             std::perror("Error: could not create a new file");
-            std::fclose(outfile);
+            outfile.close();
             return (1);
         }
         std::string buffer;
-        while (true)
+        while (std::getline(infile, buffer))
         {
-            getline(file, buffer);
-            char *ptr = buffer.find(av[2])
-            while (ptr != std::string::npos)
+            size_t pos = buffer.find(av[2]);
+            while (pos != std::string::npos)
             {
-                buffer.
-                buffer.search();
-
+                buffer.erase(pos, strlen(av[2]));
+                buffer.insert(pos, av[3]);
+                pos = buffer.find(av[2]);
             }
+            outfile << buffer << std::endl;
         }
-        
+        infile.close();
+        outfile.close();
     }
+    std::cout << "Wrong number of arguments" << std::endl;
+    return (0);
 }
