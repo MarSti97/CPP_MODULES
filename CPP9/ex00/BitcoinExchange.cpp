ffdf54d73e5f33e63b0e5aed2b94f374f343ce3d
katarina gajic <Katarina1@MacBook-Air.local>
2023-07-24 21:16:36 +0100

    CPP9 ex00, database stored in map, basic almost done, find closest date is biggest issue

--- CPP9/ex00/BitcoinExchange.cpp
+++ CPP9/ex00/BitcoinExchange.cpp
@@ -2,11 +2,11 @@
 
 BitcoinExchange::BitcoinExchange() {}
 
-BitcoinExchange::BitcoinExchange(const BitcoinExchange& copy); //make
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& copy) {(void)copy;} //make
 
 BitcoinExchange::~BitcoinExchange() {}
 
-BitcoinExchange& BitcoinExchange::operator = (const BitcoinExchange& copy); //make
+BitcoinExchange& BitcoinExchange::operator = (const BitcoinExchange& copy) {(void)copy; return *this;} //make
 
 void BitcoinExchange::exchangeRates(char* file)
 {
@@ -19,5 +19,62 @@ void BitcoinExchange::exchangeRates(char* file)
 		btc.close();
 		return ;
 	}
-	
+	std::map<std::string, double> data;
+	try {
+		makeData(data, btc);
+		makeCompare(data, input);
+	}
+	catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		input.close();
+		btc.close();
+		return ;
+	}
+}
+
+void BitcoinExchange::makeData(std::map<std::string, double>& data, std::ifstream& btc)
+{
+	std::string buffer;
+	std::string date;
+	double	price;
+	if (!std::getline(btc, buffer))
+		throw EmptyFile();
+	while (std::getline(btc, buffer))
+	{
+		getInfo(buffer, date, price);
+		data.insert(std::make_pair(date, price));
+	}
+}
+
+void BitcoinExchange::getInfo(std::string buffer, std::string& date, double& price)
+{
+	int pos = buffer.find(',');
+	date = buffer.substr(0, pos);
+	price = strtod((buffer.substr(pos + 1)).c_str(), NULL);
+}
+
+void BitcoinExchange::makeCompare(std::map<std::string, double>& data, std::ifstream& input)
+{
+	std::string buffer;
+	if (!std::getline(input, buffer))
+		throw EmptyFile();
+	while (std::getline(input, buffer))
+	{
+		try{
+			int pos = buffer.find(' ');
+			std::string key = buffer.substr(0, pos);
+			//use find to find date
+			// if the date doesnt exist make a loop and do string compare to find closest date... see if that works
+		}
+		catch (std::exception& e){
+			std::cerr << e.what() << std::endl;
+		}
+	}
+}
+
+
+BitcoinExchange::EmptyFile::EmptyFile() {}
+
+const char* BitcoinExchange::EmptyFile::what() const throw() {
+	return ("Error: File is empty");
 }
--- CPP9/ex00/BitcoinExchange.hpp
+++ CPP9/ex00/BitcoinExchange.hpp
@@ -4,6 +4,14 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <map>
+#include <cstdlib>
+
+// struct Datum {
+// 	int year;
+// 	int month;
+// 	int day;
+// };
 
 class BitcoinExchange
 {
@@ -15,7 +23,20 @@ class BitcoinExchange
 		BitcoinExchange& operator = (const BitcoinExchange& copy);
 	public :
 		static void exchangeRates(char* file);
-}
+		static void makeData(std::map<std::string, double>& data, std::ifstream& btc);
+		static void getInfo(std::string buffer, std::string& date, double& price);
+		static void makeCompare(std::map<std::string, double>& data, std::ifstream& input);
 
+	class EmptyFile : public std::exception
+	{
+		public :
+			EmptyFile();
+			virtual const char* what() const throw();
+	};
+};
+
+// Datum 	createdatum(std::string date);
+// bool	comparedatum(Datum a, Datum b);
+// Datum	findclosest(std::map<Datum, double> data, Datum find);
 
 #endif
