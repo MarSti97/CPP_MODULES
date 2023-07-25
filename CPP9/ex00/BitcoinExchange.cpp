489055460f9c87194d3723c2feaa32f35c378c00
katarina gajic <Katarina1@MacBook-Air.local>
2023-07-25 23:39:51 +0100

    small changed, fix nearest date finded, read subject wrong

--- CPP9/ex00/BitcoinExchange.cpp
+++ CPP9/ex00/BitcoinExchange.cpp
@@ -61,9 +61,16 @@ void BitcoinExchange::makeCompare(std::map<std::string, double>& data, std::ifst
 	while (std::getline(input, buffer))
 	{
 		try{
-			int pos = buffer.find(' ');
-			std::string key = buffer.substr(0, pos);
-			//use find to find date
+			std::string key = parseDate(buffer);
+			int pos = buffer.rfind(' ');
+			double value = strtod((buffer.substr(pos)).c_str(), NULL);
+			if (value < 0 || value > 1000)
+				throw ValueOutOfRange();
+			std::map<std::string, double>::iterator it = data.find(key);
+			if (it != data.end())
+				std::cout << key << " => " << value << " = " << (value * it->second) << std::endl;
+			else
+				findClosest(data, key, value);
 			// if the date doesnt exist make a loop and do string compare to find closest date... see if that works
 		}
 		catch (std::exception& e){
@@ -72,9 +79,62 @@ void BitcoinExchange::makeCompare(std::map<std::string, double>& data, std::ifst
 	}
 }
 
+std::string BitcoinExchange::parseDate(std::string buffer)
+{
+	if (buffer.find_first_not_of("0123456789.-| ") != std::string::npos)
+		throw InvalidInput();
+	if (!std::isdigit(buffer[0]) || !std::isdigit(buffer[1]) || !std::isdigit(buffer[2]) || !std::isdigit(buffer[3]) || buffer[4] != '-')
+		throw InvalidInput();
+	int month = atoi((buffer.substr(5, 6)).c_str());
+	if (buffer[7] != '-' || month < 1 || month > 12)
+		throw InvalidDate();
+	int day = atoi((buffer.substr(8, 9)).c_str());
+	if (buffer[10] != ' ' || day < 1 || day > 31)
+		throw InvalidDate();
+	if (buffer[11] != '|' || buffer[12] != ' ')
+		throw InvalidInput();
+	int pos = buffer.find(' ');
+	return (buffer.substr(0, pos));
+}
+
+void BitcoinExchange::findClosest(std::map<std::string, double>& data, std::string key, double value)
+{
+	std::map<std::string, double>::iterator it = data.begin();
+	std::map<std::string, double>::iterator best;
+	size_t min = it->first.compare(key);
+	size_t comp;
+	for (++it; it != data.end(); ++it)
+	{
+		comp = it->first.compare(key);
+		if (comp < min) {
+			min = comp;
+			best = it;
+		}
+	}
+	std::cout << it.first() << " => " << value << " = " << (value * it->second) << std::endl;
+}
+
 
 BitcoinExchange::EmptyFile::EmptyFile() {}
 
 const char* BitcoinExchange::EmptyFile::what() const throw() {
 	return ("Error: File is empty");
+}
+
+BitcoinExchange::ValueOutOfRange::ValueOutOfRange() {}
+
+const char* BitcoinExchange::ValueOutOfRange::what() const throw() {
+	return ("Error: this value is not in the range of 0 to 1000");
+}
+
+BitcoinExchange::InvalidInput::InvalidInput() {}
+
+const char* BitcoinExchange::InvalidInput::what() const throw() {
+	return ("Error: invalid input or format error");
+}
+
+BitcoinExchange::InvalidDate::InvalidDate() {}
+
+const char* BitcoinExchange::InvalidDate::what() const throw() {
+	return ("Error: invalid date or format error");
 }
--- CPP9/ex00/BitcoinExchange.hpp
+++ CPP9/ex00/BitcoinExchange.hpp
@@ -22,15 +22,38 @@ class BitcoinExchange
 
 		BitcoinExchange& operator = (const BitcoinExchange& copy);
 	public :
-		static void exchangeRates(char* file);
-		static void makeData(std::map<std::string, double>& data, std::ifstream& btc);
-		static void getInfo(std::string buffer, std::string& date, double& price);
-		static void makeCompare(std::map<std::string, double>& data, std::ifstream& input);
+		static void			exchangeRates(char* file);
+		static void			makeData(std::map<std::string, double>& data, std::ifstream& btc);
+		static void			getInfo(std::string buffer, std::string& date, double& price);
+		static void			makeCompare(std::map<std::string, double>& data, std::ifstream& input);
+		static std::string	parseDate(std::string key);
+		static void			findClosest(std::map<std::string, double>& data, std::string key, double value);
 
 	class EmptyFile : public std::exception
 	{
 		public :
 			EmptyFile();
+			virtual const char* what() const throw();
+	};
+
+	class ValueOutOfRange : public std::exception
+	{
+		public :
+			ValueOutOfRange();
+			virtual const char* what() const throw();
+	};
+
+	class InvalidInput : public std::exception
+	{
+		public :
+			InvalidInput();
+			virtual const char* what() const throw();
+	};
+
+	class InvalidDate : public std::exception
+	{
+		public :
+			InvalidDate();
 			virtual const char* what() const throw();
 	};
 };
